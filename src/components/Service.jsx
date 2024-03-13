ac20ec8e82fb933bc33e5c42d28832d54e7f422f
Gleb Mirzayev <gmirzayev@gmail.com>
2024-03-12 21:51:33 -0400

    changed to useEffect

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,17 +1,64 @@
 import "../styles/global.css";
 import rings from "../assets/rings.png";
 import cut from "../assets/cut.png";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Service() {
-  const elements = document.querySelectorAll(".pill--span-text");
-  let currentIndex = 0;
-  let timerInterval;
+  // const elements = document.querySelectorAll(".pill--span-text");
+  // let currentIndex = 0;
+  // let timerInterval;
+  // let animationTimerInterval;
+  // const textPath = useRef(null);
+  const [offset, setOffset] = useState(59.5);
+  const [itemIndex, setItemIndex] = useState();
+  const [isRunning, setIsRunning] = useState(true);
 
-  useEffect(() => {
-    requestAnimationFrame(step);
-  }, []);
+  function useInterval(callback, delay) {
+    const savedCallback = useRef();
 
+    // Remember the latest callback.
+    useEffect(() => {
+      savedCallback.current = callback;
+    }, [callback]);
+
+    // Set up the interval.
+    useEffect(() => {
+      function tick() {
+        savedCallback.current();
+      }
+      if (delay !== null) {
+        let id = setInterval(tick, delay);
+        return () => clearInterval(id);
+      }
+    }, [delay]);
+  }
+
+  useInterval(
+    () => {
+      let newOffset = offset - 0.02;
+      if (newOffset <= 0) {
+        newOffset = 59.5;
+      }
+      setOffset(newOffset);
+    },
+    isRunning ? 16 : null
+  );
+
+  // useEffect(() => {
+  //   const animationInterval = setInterval(() => {
+  //     setOffset((prevOffset) => {
+  //       // const newOffset = prevOffset - 0.01;
+  //       if (prevOffset - 0.01 <= 0) {
+  //         return 59.5;
+  //       } else {
+  //         return prevOffset - 0.01;
+  //       }
+  //     });
+  //     // console.log(offset);
+  //   }, 100);
+  //   return () => clearInterval(animationInterval);
+  // }, []);
+  /*
   // const [scrolling, setScrolling] = useState(true);
   let scrolling = true;
 
@@ -48,7 +95,34 @@ export default function Service() {
   }
 
   // Start the timer
-  startTimer();
+  // startTimer();
+
+  let text = document.querySelector("#text-path");
+  if (text) {
+    text.setAttribute("startOffset", "0%");
+    // text1.setAttribute("startOffset", "6%");
+  }
+
+  // function startAnimationTimer() {
+  //   animationTimerInterval = setInterval(function () {
+  //     offset -= 0.01;
+
+  //     if (offset <= 0) {
+  //       offset = 59.5;
+  //     }
+
+  //     if (text) {
+  //       text.setAttribute("startOffset", offset + "%");
+  //     }
+  //   }, 60);
+  // }
+
+  // function pauseAnimationTimer() {
+  //   clearInterval(animationTimerInterval);
+  // }
+
+  // Start the timer
+  // startAnimationTimer();
 
   // Event listeners for hover
   elements.forEach((element, index) => {
@@ -63,13 +137,6 @@ export default function Service() {
     });
   });
 
-  let offset = 0;
-  let text = document.querySelector("#text-path");
-  if (text) {
-    text.setAttribute("startOffset", "0%");
-    // text1.setAttribute("startOffset", "6%");
-  }
-
   function spin() {
     let image1 = document.querySelector("#first-image");
     // let image2 = document.querySelector("#second-image");
@@ -82,25 +149,25 @@ export default function Service() {
     }
   }
 
-  function step() {
-    // if (scrolling) {
-    offset -= 0.01;
-    // console.log(offset);
+  // function step() {
+  //   // if (scrolling) {
+  //   offset -= 0.01;
+  //   // console.log(offset);
 
-    if (offset <= 0) {
-      offset = 59.5;
-    }
+  //   if (offset <= 0) {
+  //     offset = 59.5;
+  //   }
 
-    if (text) {
-      text.setAttribute("startOffset", offset + "%");
-    }
+  //   if (text) {
+  //     text.setAttribute("startOffset", offset + "%");
+  //   }
 
-    requestAnimationFrame(step);
-    // }
-  }
+  //   requestAnimationFrame(step);
+  //   // }
+  // }
 
   // window.requestAnimationFrame(step);
-
+*/
   return (
     <>
       <div className="pill--container">
@@ -125,8 +192,11 @@ export default function Service() {
               stroke="none"
               d="M 131.2244 5.251 H 131.2244 S 259.7939 5.251 259.7939 133.8205 V 302.432 S 259.7939 431.0015 131.2244 431.0015 H 131.2244 S 2.6549 431.0015 2.6549 302.432 V 133.8205 S 2.6549 5.251 131.2244 5.251 H 131.2244 S 259.7939 5.251 259.7939 133.8205 V 302.432 S 259.7939 431.0015 131.2244 431.0015 H 131.2244 S 2.6549 431.0015 2.6549 302.432"
             />
-            <text>
-              <textPath href="#path" id="text-path">
+            <text
+              onMouseEnter={() => setIsRunning(false)}
+              onMouseLeave={() => setIsRunning(true)}
+            >
+              <textPath href="#path" id="text-path" startOffset={offset + "%"}>
                 By Hart is a <tspan className="pill--span-text">story</tspan> +
                 <tspan className="pill--span-text"> identity</tspan> +
                 <tspan className="pill--span-text"> art</tspan> +
@@ -134,6 +204,14 @@ export default function Service() {
                 <tspan className="pill--span-text"> content</tspan> +
                 <tspan className="pill--span-text"> experience</tspan> +
                 actually creative studio by Sarah Hartley.
+                {/* <animate
+                  attributeName="startOffset"
+                  from="0%"
+                  to="59.5%"
+                  begin="0s"
+                  dur="20s"
+                  repeatCount="indefinite"
+                /> */}
               </textPath>
             </text>
           </svg>
