35a314ecfb7993e025c0e201313fd0b2c78c4ac4
Gleb Mirzayev <gmirzayev@gmail.com>
2024-03-13 15:05:56 -0400

    rotate pill on timer and click

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -4,14 +4,11 @@ import cut from "../assets/cut.png";
 import { useEffect, useRef, useState } from "react";
 
 export default function Service() {
-  // const elements = document.querySelectorAll(".pill--span-text");
-  // let currentIndex = 0;
-  // let timerInterval;
-  // let animationTimerInterval;
-  // const textPath = useRef(null);
   const [offset, setOffset] = useState(59.5);
-  const [itemIndex, setItemIndex] = useState();
   const [isRunning, setIsRunning] = useState(true);
+  const [isHighlighting, setIsHighlighting] = useState(true);
+  const [highlightedElement, setHighlightedElement] = useState(0);
+  const [animatedPosition, setAnimatedPosition] = useState(0);
 
   function useInterval(callback, delay) {
     const savedCallback = useRef();
@@ -44,147 +41,28 @@ export default function Service() {
     isRunning ? 16 : null
   );
 
-  // useEffect(() => {
-  //   const animationInterval = setInterval(() => {
-  //     setOffset((prevOffset) => {
-  //       // const newOffset = prevOffset - 0.01;
-  //       if (prevOffset - 0.01 <= 0) {
-  //         return 59.5;
-  //       } else {
-  //         return prevOffset - 0.01;
-  //       }
-  //     });
-  //     // console.log(offset);
-  //   }, 100);
-  //   return () => clearInterval(animationInterval);
-  // }, []);
-  /*
-  // const [scrolling, setScrolling] = useState(true);
-  let scrolling = true;
+  useInterval(
+    () => {
+      if (isHighlighting) {
+        setHighlightedElement((highlightedElement + 1) % 6);
+        setAnimatedPosition((highlightedElement + 1) % 6);
+      }
+    },
+    isHighlighting ? 5000 : null
+  );
 
-  function clearHighlight(index) {
-    if (elements[index]) {
-      elements[index].classList.remove("highlight");
-      // cards[index].classList.remove("card-show");
-    }
+  function handleHighlight(index) {
+    setIsHighlighting(false);
+    setHighlightedElement(index);
   }
 
-  function highlightElement(index) {
-    if (elements[index]) {
-      // Highlight the current element
-      elements[index].classList.add("highlight");
-      // cards[index].classList.add("card-show");
-
-      // Update the currentIndex
-      currentIndex = index;
-    }
-  }
-
-  highlightElement(currentIndex);
-
-  function startTimer() {
-    timerInterval = setInterval(function () {
-      clearHighlight(currentIndex);
-      currentIndex = (currentIndex + 1) % elements.length;
-      highlightElement(currentIndex);
-    }, 5000);
-  }
-
-  function pauseTimer() {
-    clearInterval(timerInterval);
-  }
-
-  // Start the timer
-  // startTimer();
-
-  let text = document.querySelector("#text-path");
-  if (text) {
-    text.setAttribute("startOffset", "0%");
-    // text1.setAttribute("startOffset", "6%");
-  }
-
-  // function startAnimationTimer() {
-  //   animationTimerInterval = setInterval(function () {
-  //     offset -= 0.01;
-
-  //     if (offset <= 0) {
-  //       offset = 59.5;
-  //     }
-
-  //     if (text) {
-  //       text.setAttribute("startOffset", offset + "%");
-  //     }
-  //   }, 60);
-  // }
-
-  // function pauseAnimationTimer() {
-  //   clearInterval(animationTimerInterval);
-  // }
-
-  // Start the timer
-  // startAnimationTimer();
-
-  // Event listeners for hover
-  elements.forEach((element, index) => {
-    element.addEventListener("mouseenter", function () {
-      pauseTimer(); // Pause the timer when hovering
-      clearHighlight(currentIndex);
-      highlightElement(index); // Highlight the hovered element
-    });
-
-    element.addEventListener("mouseleave", function () {
-      startTimer(); // Resume the timer when not hovering
-    });
-  });
-
-  function spin() {
-    let image1 = document.querySelector("#first-image");
-    // let image2 = document.querySelector("#second-image");
-    let rotateText = document.querySelector("#container1");
-
-    if (image1 && rotateText) {
-      image1.classList.add("fade");
-      // image2.classList.add("spin");
-      rotateText.classList.add("spin2");
-    }
-  }
-
-  // function step() {
-  //   // if (scrolling) {
-  //   offset -= 0.01;
-  //   // console.log(offset);
-
-  //   if (offset <= 0) {
-  //     offset = 59.5;
-  //   }
-
-  //   if (text) {
-  //     text.setAttribute("startOffset", offset + "%");
-  //   }
-
-  //   requestAnimationFrame(step);
-  //   // }
-  // }
-
-  // window.requestAnimationFrame(step);
-*/
   return (
     <>
-      <div className="pill--container">
-        <div
-          className="pill"
-          // onMouseEnter={(e) => {
-          //   scrolling = false;
-          //   // console.log(scrolling);
-          //   console.log(e);
-          // }}
-          // onMouseLeave={(e) => {
-          //   scrolling = true;
-          //   // console.log(scrolling);
-          //   console.log(e);
-          //   // step();
-          // }}
-        >
+      <div
+        className="pill--container"
+        style={{ transform: `rotate(${animatedPosition * 20}deg)` }}
+      >
+        <div className="pill">
           <svg viewBox="0 80 250 100" xmlns="http://www.w3.org/2000/svg">
             <path
               id="path"
@@ -197,21 +75,78 @@ export default function Service() {
               onMouseLeave={() => setIsRunning(true)}
             >
               <textPath href="#path" id="text-path" startOffset={offset + "%"}>
-                By Hart is a <tspan className="pill--span-text">story</tspan> +
-                <tspan className="pill--span-text"> identity</tspan> +
-                <tspan className="pill--span-text"> art</tspan> +
-                <tspan className="pill--span-text"> design</tspan> +
-                <tspan className="pill--span-text"> content</tspan> +
-                <tspan className="pill--span-text"> experience</tspan> +
-                actually creative studio by Sarah Hartley.
-                {/* <animate
-                  attributeName="startOffset"
-                  from="0%"
-                  to="59.5%"
-                  begin="0s"
-                  dur="20s"
-                  repeatCount="indefinite"
-                /> */}
+                By Hart is a{" "}
+                <tspan
+                  className={`pill--span-text ${
+                    highlightedElement == 0 ? "highlighted" : ""
+                  }`}
+                  onMouseEnter={() => handleHighlight(0)}
+                  onMouseLeave={() => setIsHighlighting(true)}
+                  onClick={() => setAnimatedPosition(0)}
+                >
+                  story
+                </tspan>{" "}
+                +
+                <tspan
+                  className={`pill--span-text ${
+                    highlightedElement == 1 ? "highlighted" : ""
+                  }`}
+                  onMouseEnter={() => handleHighlight(1)}
+                  onMouseLeave={() => setIsHighlighting(true)}
+                  onClick={() => setAnimatedPosition(1)}
+                >
+                  {" "}
+                  identity
+                </tspan>{" "}
+                +
+                <tspan
+                  className={`pill--span-text ${
+                    highlightedElement == 2 ? "highlighted" : ""
+                  }`}
+                  onMouseEnter={() => handleHighlight(2)}
+                  onMouseLeave={() => setIsHighlighting(true)}
+                  onClick={() => setAnimatedPosition(2)}
+                >
+                  {" "}
+                  art
+                </tspan>{" "}
+                +
+                <tspan
+                  className={`pill--span-text ${
+                    highlightedElement == 3 ? "highlighted" : ""
+                  }`}
+                  onMouseEnter={() => handleHighlight(3)}
+                  onMouseLeave={() => setIsHighlighting(true)}
+                  onClick={() => setAnimatedPosition(3)}
+                >
+                  {" "}
+                  design
+                </tspan>{" "}
+                +
+                <tspan
+                  className={`pill--span-text ${
+                    highlightedElement == 4 ? "highlighted" : ""
+                  }`}
+                  onMouseEnter={() => handleHighlight(4)}
+                  onMouseLeave={() => setIsHighlighting(true)}
+                  onClick={() => setAnimatedPosition(4)}
+                >
+                  {" "}
+                  content
+                </tspan>{" "}
+                +
+                <tspan
+                  className={`pill--span-text ${
+                    highlightedElement == 5 ? "highlighted" : ""
+                  }`}
+                  onMouseEnter={() => handleHighlight(5)}
+                  onMouseLeave={() => setIsHighlighting(true)}
+                  onClick={() => setAnimatedPosition(5)}
+                >
+                  {" "}
+                  experience
+                </tspan>{" "}
+                + actually creative studio by Sarah Hartley.
               </textPath>
             </text>
           </svg>
@@ -229,5 +164,3 @@ export default function Service() {
     </>
   );
 }
-
-// By Hart is a story + identity + art + design + content + experience + actually creative studio.
