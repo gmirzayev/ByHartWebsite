79c841ab2ca3cd7dc68cd3971fa5dc6c43c1b6f1
Christian Swinehart <drafting@samizdat.co>
2025-03-05 14:38:47 -0500

    include tailwind postcss plugin

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,5 +1,6 @@
 module.exports = {
     plugins: [
+        require('tailwindcss'),
         require('autoprefixer'),
         require("@csstools/postcss-is-pseudo-class"),
         require("postcss-nesting"),
